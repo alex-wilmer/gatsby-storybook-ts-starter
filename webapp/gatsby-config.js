1e6345eaa430915bc06097dc82267bbcbae1198d
Alex Wilmer <djazium@gmail.com>
2021-05-25 12:33:06 -0400

    init

---
+++ webapp/gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: "ControlD",
+    description: "Public website and control panel frontend for ControlD",
+  },
+  plugins: ["gatsby-plugin-theme-ui"],
+};
